85462e9b755d3da11b3d71a726fc3d9c902dbce6
Jibe7 <jb7_chevrier@hotmail.com>
2024-05-02 21:29:55 +0200

    changed base in viteconfig

--- first-react-project/vite.config.js
+++ first-react-project/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test : {
-    base: "/2024-LearnReact/",
+    base: "/2024-LearnReact/first-react-project",
     globals: true,
     environment: 'jsdom',
     css: true,
