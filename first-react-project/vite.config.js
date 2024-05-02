84a22e72a9786e30f30a81e3b6c71f45d4886b98
Jibe7 <jb7_chevrier@hotmail.com>
2024-05-02 23:51:43 +0200

    vite config

--- first-react-project/vite.config.js
+++ first-react-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/2024-LearnReact/first-react-project",
+  base: "/2024-LearnReact/",
   plugins: [react()],
   test : {
     globals: true,
